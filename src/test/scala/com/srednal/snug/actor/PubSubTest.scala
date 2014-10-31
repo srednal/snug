a10531a592107bcac9a7a030e4b11d78045c29b7
Dave Landers <dave.landers@emc.com>
2014-10-31 15:57:38 -0600

    Scala 2.11.4, and fix some resulting issues.  Bump timings in pubsub test.

--- src/test/scala/com/srednal/snug/actor/PubSubTest.scala
+++ src/test/scala/com/srednal/snug/actor/PubSubTest.scala
@@ -1,6 +1,6 @@
 package com.srednal.snug.actor
 
-import akka.actor.{Actor, ActorRef, ActorSystem}
+import akka.actor.{Actor, ActorSystem}
 import akka.actor.ActorDSL._
 import akka.testkit.TestActorRef
 import org.scalatest._
@@ -27,36 +27,36 @@ class PubSubTest extends WordSpec with Matchers {
     }
 
     "reply Subscribed" in {
-      Await.result(rootActor ? Subscribe(emptyActor, None), 100.millis) shouldBe Subscribed
-      Await.result(rootActor ? Subscribe(emptyActor, None), 100.millis) shouldBe Subscribed
+      Await.result(rootActor ? Subscribe(emptyActor, None), 1.second) shouldBe Subscribed
+      Await.result(rootActor ? Subscribe(emptyActor, None), 1.second) shouldBe Subscribed
     }
     "reply Unsubscribed" in {
-      Await.result(rootActor ? Unsubscribe(emptyActor, None), 100.millis) shouldBe Unsubscribed
+      Await.result(rootActor ? Unsubscribe(emptyActor, None), 1.second) shouldBe Unsubscribed
     }
 
     "reply Subscribed for sub-channels" in {
-      Await.result(rootActor ? Subscribe(emptyActor, Some("outer")), 100.millis) shouldBe Subscribed
+      Await.result(rootActor ? Subscribe(emptyActor, Some("outer")), 1.second) shouldBe Subscribed
     }
     "reply Subscribed for sub-sub-channels" in {
-      Await.result(rootActor ? Subscribe(emptyActor, Some("outer/inner")), 100.millis) shouldBe Subscribed
+      Await.result(rootActor ? Subscribe(emptyActor, Some("outer/inner")), 1.second) shouldBe Subscribed
     }
 
     "reply Unsubscribed for sub-channels" in {
-      Await.result(rootActor ? Unsubscribe(emptyActor, Some("outer")), 100.millis) shouldBe Unsubscribed
+      Await.result(rootActor ? Unsubscribe(emptyActor, Some("outer")), 1.second) shouldBe Unsubscribed
     }
     "reply Unsubscribed for sub-sub-channels" in {
-      Await.result(rootActor ? Unsubscribe(emptyActor, Some("outer/inner")), 100.millis) shouldBe Unsubscribed
+      Await.result(rootActor ? Unsubscribe(emptyActor, Some("outer/inner")), 1.second) shouldBe Unsubscribed
     }
 
     "reply subscribed from subscribe func" in {
       implicit val rx = inbox()
       subscribe(emptyActor, Some("outer/inner"))
-      rx.receive(100.millis) shouldBe Subscribed
+      rx.receive(1.second) shouldBe Subscribed
     }
     "reply unsubscribed from unsubscribe func" in {
       implicit val rx = inbox()
       unsubscribe(emptyActor, Some("outer/inner"))
-      rx.receive(100.millis) shouldBe Unsubscribed
+      rx.receive(1.second) shouldBe Unsubscribed
     }
   }
 
@@ -70,41 +70,41 @@ class PubSubTest extends WordSpec with Matchers {
     "create a sub-channel actor" in {
       val rootRef = TestActorRef(new PubSub(true), "pubsub")
       val root = rootRef.underlyingActor
-      Await.ready(root.channelActor("foo", false), 100.millis).value.get shouldBe a[Failure[_]]
-      Await.ready(root.channelActor("foo", true), 100.millis).value.get shouldBe a[Success[ActorRef]]
-      Await.ready(root.channelActor("foo", false), 100.millis).value.get shouldBe a[Success[ActorRef]]
-      Await.ready(root.channelActor("foo", false), 100.millis).value.get.get.path shouldBe (rootRef.path / "foo")
+      Await.ready(root.channelActor("foo", false), 1.second).value.get shouldBe a[Failure[_]]
+      Await.ready(root.channelActor("foo", true), 1.second).value.get shouldBe a[Success[_]]
+      Await.ready(root.channelActor("foo", false), 1.second).value.get shouldBe a[Success[_]]
+      Await.ready(root.channelActor("foo", false), 1.second).value.get.get.path shouldBe (rootRef.path / "foo")
     }
 
     "deliver messages to subscribed actors" in {
       val listener = inbox()
-      Await.ready(rootActor ? Subscribe(listener.receiver, None), 100.millis)
+      Await.ready(rootActor ? Subscribe(listener.receiver, None), 1.second)
       rootActor ! Message("hello there", None)
-      listener.receive(100.millis) shouldBe "hello there"
+      listener.receive(1.second) shouldBe "hello there"
     }
 
     "stop delivery to unsubscribed actors" in {
       val listener = inbox()
-      Await.ready(rootActor ? Subscribe(listener.receiver, None), 100.millis)
+      Await.ready(rootActor ? Subscribe(listener.receiver, None), 1.second)
 
       rootActor ! Message("hello there you", None)
-      listener.receive(100.millis) shouldBe "hello there you"
+      listener.receive(1.second) shouldBe "hello there you"
 
       rootActor ! Message("hello there more", None)
-      listener.receive(100.millis) shouldBe "hello there more"
+      listener.receive(1.second) shouldBe "hello there more"
 
-      Await.ready(rootActor ? Unsubscribe(listener.receiver, None), 100.millis)
+      Await.ready(rootActor ? Unsubscribe(listener.receiver, None), 1.second)
 
       rootActor ! Message("hello there again", None)
-      a[TimeoutException] should be thrownBy listener.receive(100.millis)
+      a[TimeoutException] should be thrownBy listener.receive(250.millis)
     }
 
 
     "deliver root messages to subscribed actors in sub-channels" in {
       val listener = inbox()
-      Await.ready(rootActor ? Subscribe(listener.receiver, Some("test/sub")), 100.millis)
+      Await.ready(rootActor ? Subscribe(listener.receiver, Some("test/sub")), 1.second)
       rootActor ? Message("hello subs", Some("test"))
-      listener.receive(100.millis) shouldBe "hello subs"
+      listener.receive(1.second) shouldBe "hello subs"
     }
 
     "subscribing to a sub-channel implies subscription to parents also" in {
@@ -112,16 +112,16 @@ class PubSubTest extends WordSpec with Matchers {
       val listener1 = inbox()
       val listener2 = inbox()
       val listener3 = inbox()
-      Await.ready(rootActor ? Subscribe(listener0.receiver, None), 100.millis)
-      Await.ready(rootActor ? Subscribe(listener1.receiver, Some("test1")), 100.millis)
-      Await.ready(rootActor ? Subscribe(listener2.receiver, Some("test1/sub1")), 100.millis)
-      Await.ready(rootActor ? Subscribe(listener3.receiver, Some("test1/sub1/sub2")), 100.millis)
+      Await.ready(rootActor ? Subscribe(listener0.receiver, None), 1.second)
+      Await.ready(rootActor ? Subscribe(listener1.receiver, Some("test1")), 1.second)
+      Await.ready(rootActor ? Subscribe(listener2.receiver, Some("test1/sub1")), 1.second)
+      Await.ready(rootActor ? Subscribe(listener3.receiver, Some("test1/sub1/sub2")), 1.second)
 
       rootActor ? Message("hello top", None)
-      listener0.receive(100.millis) shouldBe "hello top"
-      listener1.receive(100.millis) shouldBe "hello top"
-      listener2.receive(100.millis) shouldBe "hello top"
-      listener3.receive(100.millis) shouldBe "hello top"
+      listener0.receive(1.second) shouldBe "hello top"
+      listener1.receive(1.second) shouldBe "hello top"
+      listener2.receive(1.second) shouldBe "hello top"
+      listener3.receive(1.second) shouldBe "hello top"
     }
 
     "publish to a sub-channel does not deliver to parents" in {
@@ -129,16 +129,16 @@ class PubSubTest extends WordSpec with Matchers {
       val listener1 = inbox()
       val listener2 = inbox()
       val listener3 = inbox()
-      Await.ready(rootActor ? Subscribe(listener0.receiver, None), 100.millis)
-      Await.ready(rootActor ? Subscribe(listener1.receiver, Some("test2")), 100.millis)
-      Await.ready(rootActor ? Subscribe(listener2.receiver, Some("test2/sub1")), 100.millis)
-      Await.ready(rootActor ? Subscribe(listener3.receiver, Some("test2/sub1/sub2")), 100.millis)
+      Await.ready(rootActor ? Subscribe(listener0.receiver, None), 1.second)
+      Await.ready(rootActor ? Subscribe(listener1.receiver, Some("test2")), 1.second)
+      Await.ready(rootActor ? Subscribe(listener2.receiver, Some("test2/sub1")), 1.second)
+      Await.ready(rootActor ? Subscribe(listener3.receiver, Some("test2/sub1/sub2")), 1.second)
 
       rootActor ? Message("hello down", Some("test2/sub1"))
-      listener2.receive(100.millis) shouldBe "hello down"
-      listener3.receive(100.millis) shouldBe "hello down"
-      a[TimeoutException] should be thrownBy listener0.receive(100.millis)
-      a[TimeoutException] should be thrownBy listener1.receive(100.millis)
+      listener2.receive(1.second) shouldBe "hello down"
+      listener3.receive(1.second) shouldBe "hello down"
+      a[TimeoutException] should be thrownBy listener0.receive(250.millis)
+      a[TimeoutException] should be thrownBy listener1.receive(250.millis)
     }
   }
 }
