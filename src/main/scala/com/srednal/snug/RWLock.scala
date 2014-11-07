b118e4ee2de9df0f371d21b1fdfc068c2b05fbb1
Dave Landers <dave.landers@emc.com>
2014-11-07 07:48:39 -0700

    a read/write lock

---
+++ src/main/scala/com/srednal/snug/RWLock.scala
@@ -0,0 +1,30 @@
+package com.srednal.snug
+
+import java.util.concurrent.locks.ReentrantReadWriteLock
+
+object RWLock {
+  def apply() = new RWLock
+}
+
+class RWLock {
+  val lock = new ReentrantReadWriteLock
+
+  def read[A](f: => A): A = {
+    lock.readLock().lock()
+    try f
+    finally lock.readLock().unlock()
+  }
+
+  def write[A](f: => A): A = {
+    // you can not obtain a write lock while a read lock is held - even if you are the holder of the read lock (self-deadlock)
+    // so let's work around that
+    val readLocks = lock.getReadHoldCount
+    0 until readLocks foreach { _ => lock.readLock().unlock()}
+    lock.writeLock().lock()
+    // now we can reo-btain those read locks and restore balance
+    0 until readLocks foreach { _ => lock.readLock().lock()}
+
+    try f
+    finally lock.writeLock().unlock()
+  }
+}
