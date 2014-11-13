7b04cdbedf7a2f654b8fd60a79ae7f8a215c143e
Dave Landers <dave.landers@emc.com>
2014-11-13 11:41:15 -0700

    update logger tests in order to experiment with scalamock

--- src/main/scala/com/srednal/snug/log/Logger.scala
+++ src/main/scala/com/srednal/snug/log/Logger.scala
@@ -4,8 +4,9 @@ import com.srednal.snug.ServiceLoader
 
 
 trait Logger {
-  def trace(m: => String, t: Option[Throwable] = None): Unit
+  def trace(m: => String, t: Option[Throwable]): Unit
   def trace(m: => String, t: Throwable): Unit = trace(m, Some(t))
+//  def trace(m: => String): Unit = trace(m, None)
   def trace(x: => Any): Unit = trace(x.toString, None)
 
   def tracing[T](f: => T): T = {
@@ -16,6 +17,7 @@ trait Logger {
 
   def debug(m: => String, t: Option[Throwable] = None): Unit
   def debug(m: => String, t: Throwable): Unit = debug(m, Some(t))
+//  def debug(m: => String): Unit = debug(m, None)
   def debug(x: => Any): Unit = debug(x.toString, None)
 
   def debuging[T](f: => T): T = {
@@ -24,19 +26,22 @@ trait Logger {
     m
   }
 
-  def info(m: => String, t: Option[Throwable] = None): Unit
+  def info(m: => String, t: Option[Throwable]): Unit
   def info(m: => String, t: Throwable): Unit = info(m, Some(t))
+  def info(m: => String): Unit = info(m, None)
 
-  def warn(m: => String, t: Option[Throwable] = None): Unit
+  def warn(m: => String, t: Option[Throwable]): Unit
   def warn(m: => String, t: Throwable): Unit = warn(m, Some(t))
+  def warn(m: => String): Unit = warn(m, None)
 
-  def error(m: => String, t: Option[Throwable] = None): Unit
+  def error(m: => String, t: Option[Throwable]): Unit
   def error(m: => String, t: Throwable): Unit = error(m, Some(t))
+  def error(m: => String): Unit = error(m, None)
 }
 
-/* This is split out so tests can direct to a test Logger instance */
-private[log] trait LoggerMaker {
-  val factory: String => Logger
+
+object Logger {
+  val factory: String => Logger = ServiceLoader[LoggerFactory] getOrElse { _: String => Silent}
 
   def apply(thiz: Any): Logger = apply(thiz.getClass.getName)
 
@@ -45,11 +50,6 @@ private[log] trait LoggerMaker {
     case objName if objName.last == '$' => apply(objName.init) // allows object Foo { val logger = Logger(this) }
     case n => factory(n)
   }
-}
-
-
-object Logger extends LoggerMaker {
-  override val factory: String => Logger = ServiceLoader[LoggerFactory] getOrElse { _: String => Logger.Silent}
 
   object Silent extends Logger {
     def trace(m: => String, t: Option[Throwable]) = ()
@@ -58,8 +58,8 @@ object Logger extends LoggerMaker {
     def warn(m: => String, t: Option[Throwable]) = ()
     def error(m: => String, t: Option[Throwable]) = ()
   }
-}
 
+}
 
 trait LoggerFactory extends (String => Logger) {
   def apply(n: String): Logger
--- src/test/scala/com/srednal/snug/log/LoggerTest.scala
+++ src/test/scala/com/srednal/snug/log/LoggerTest.scala
@@ -1,112 +1,32 @@
 package com.srednal.snug.log
 
+import org.scalamock.scalatest.MockFactory
 import org.scalatest._
-import scala.collection.mutable
-
-class TestLogger(val name: String) extends Logger {
-  val out = mutable.Buffer[(String, String, Option[Throwable])]()
-  override def trace(m: => String, t: Option[Throwable]) = out += (("t", m, t))
-  override def debug(m: => String, t: Option[Throwable]) = out += (("d", m, t))
-  override def info(m: => String, t: Option[Throwable]) = out += (("i", m, t))
-  override def warn(m: => String, t: Option[Throwable]) = out += (("w", m, t))
-  override def error(m: => String, t: Option[Throwable]) = out += (("e", m, t))
-}
+import org.slf4j
 
 object LoggerTest {
-  val lm = new LoggerMaker {
-    override val factory = new TestLogger(_)
-  }
-  val testObjectLogger = lm(this)
+  val testObjectLogger = Logger(this)
 }
 
-class LoggerTest extends WordSpec with Matchers {
+class LoggerTest extends WordSpec with Matchers with MockFactory {
   import LoggerTest._
 
-  "The LoggerMaker" should {
+  "The Logger object" should {
     "create a logger for a name" in {
-      lm("foo.bar") should have('name("foo.bar"))
+      Logger("foo.bar") should have('name("foo.bar"))
     }
     "create a logger for a class" in {
-      lm(this) should have('name("com.srednal.snug.log.LoggerTest"))
+      Logger(this) should have('name("com.srednal.snug.log.LoggerTest"))
     }
     "create a logger for an object" in {
       testObjectLogger should have('name("com.srednal.snug.log.LoggerTest"))
     }
-    "create a root logger with empty name" in {
-      lm("") should have('name(""))
-      lm(null) should have('name(""))
-    }
     "use slf4j by default" in {
       Logger(this) shouldBe a[Slf4jLogger]
     }
-  }
-
-  "a Logger" should {
-    "allow messages to be lazy" in {
-      var evaluated: String = ""
-
-      Logger.Silent.trace({evaluated = "trace"; "omg"})
-      Logger.Silent.trace({evaluated = "trace1"; "omg"}, new Throwable)
-      Logger.Silent.trace({evaluated = "trace2"; this})
-
-      Logger.Silent.debug({evaluated = "debug"; "omg"})
-      Logger.Silent.debug({evaluated = "debug1"; "omg"}, new Throwable)
-      Logger.Silent.debug({evaluated = "debug2"; this})
-
-      Logger.Silent.info({evaluated = "info"; "omg"})
-      Logger.Silent.info({evaluated = "info1"; "omg"}, new Throwable)
-
-      Logger.Silent.warn({evaluated = "warn"; "omg"})
-      Logger.Silent.warn({evaluated = "warn1"; "omg"}, new Throwable)
-
-      Logger.Silent.error({evaluated = "error"; "omg"})
-      Logger.Silent.error({evaluated = "error1"; "omg"}, new Throwable)
-
-      evaluated shouldBe ""
-    }
-
-    "log trace" in {
-      val l = lm("tracer")
-      val t = new Throwable
-      l.trace("foo")
-      l.trace("bar", t)
-      l.trace(this)
-      l.tracing{
-        "this is the stuff"
-      } shouldBe "this is the stuff"
-      l should have('out(Seq(("t", "foo", None), ("t", "bar", Some(t)), ("t", this.toString(), None),("t", "this is the stuff", None))))
-    }
-    "log debug" in {
-      val l = lm("debuger")
-      val t = new Throwable
-      l.debug("foo")
-      l.debug("bar", t)
-      l.debug(this)
-      l.debuging{
-        "this is the stuff"
-      } shouldBe "this is the stuff"
-      l should have('out(Seq(("d", "foo", None), ("d", "bar", Some(t)), ("d", this.toString(), None),("d", "this is the stuff", None))))
-    }
-    "log info" in {
-      val l = lm("infoer")
-      val t = new Throwable
-      l.info("foo")
-      l.info("bar", t)
-      l should have('out(Seq(("i", "foo", None), ("i", "bar", Some(t)))))
-    }
-    "log warn" in {
-      val l = lm("warner")
-      val t = new Throwable
-      l.warn("foo")
-      l.warn("bar", t)
-      l should have('out(Seq(("w", "foo", None), ("w", "bar", Some(t)))))
-    }
-    "log error" in {
-      val l = lm("errorer")
-      val t = new Throwable
-      l.error("foo")
-      l.error("bar", t)
-      l should have('out(Seq(("e", "foo", None), ("e", "bar", Some(t)))))
+    "create a slf4j root logger from an empty name" in {
+      Logger("") should have('name(org.slf4j.Logger.ROOT_LOGGER_NAME))
+      Logger(null) should have('name(org.slf4j.Logger.ROOT_LOGGER_NAME))
     }
   }
 
@@ -118,14 +38,272 @@ class LoggerTest extends WordSpec with Matchers {
     "create a root logger" in {
       new Slf4jLoggerFactory()("") should have('name(org.slf4j.Logger.ROOT_LOGGER_NAME))
     }
-    "log without errors" in {
-      // in lieu of testing that the logging does something (trust the fwk)
-      val l = new Slf4jLoggerFactory()("foo.bar")
-      l.trace("foo", Some(new Throwable))
-      l.debug("foo", Some(new Throwable))
-      l.info("foo", Some(new Throwable))
-      l.warn("foo", Some(new Throwable))
-      l.error("foo", Some(new Throwable))
+    "log via slf4j without errors" in {
+      // in lieu of testing that the logging does something (trust the framework to do the right thing)
+      val log = new Slf4jLoggerFactory().apply("foo.bar")
+      log.trace("foo", Some(new Throwable))
+      log.debug("foo", Some(new Throwable))
+      log.info("foo", Some(new Throwable))
+      log.warn("foo", Some(new Throwable))
+      log.error("foo", Some(new Throwable))
+    }
+  }
+
+  "the Slf4Logger" should {
+    "trace" in {
+      val log = new Slf4jLogger("test") {
+        override val l = mock[slf4j.Logger]
+      }
+
+      val t1 = new Throwable("t1")
+
+      (log.l.isTraceEnabled: () => Boolean).expects().returning(true)
+      (log.l.trace: (String, java.lang.Throwable) => Unit).expects("foo", t1)
+
+      log.trace("foo", t1)
+
+      (log.l.isTraceEnabled: () => Boolean).expects().returning(true)
+      (log.l.trace: (String, java.lang.Throwable) => Unit).expects("bar", null)
+
+      log.trace("bar")
+
+      (log.l.isTraceEnabled: () => Boolean).expects().returning(false)
+      log.trace("baz")
+    }
+    "debug" in {
+      val log = new Slf4jLogger("test") {
+        override val l = mock[slf4j.Logger]
+      }
+
+      val t1 = new Throwable("t1")
+
+      (log.l.isDebugEnabled: () => Boolean).expects().returning(true)
+      (log.l.debug: (String, java.lang.Throwable) => Unit).expects("foo", t1)
+
+      log.debug("foo", t1)
+
+      (log.l.isDebugEnabled: () => Boolean).expects().returning(true)
+      (log.l.debug: (String, java.lang.Throwable) => Unit).expects("bar", null)
+
+      log.debug("bar")
+
+      (log.l.isDebugEnabled: () => Boolean).expects().returning(false)
+      log.debug("baz")
+    }
+    "info" in {
+      val log = new Slf4jLogger("test") {
+        override val l = mock[slf4j.Logger]
+      }
+
+      val t1 = new Throwable("t1")
+
+      (log.l.isInfoEnabled: () => Boolean).expects().returning(true)
+      (log.l.info: (String, java.lang.Throwable) => Unit).expects("foo", t1)
+
+      log.info("foo", t1)
+
+      (log.l.isInfoEnabled: () => Boolean).expects().returning(true)
+      (log.l.info: (String, java.lang.Throwable) => Unit).expects("bar", null)
+
+      log.info("bar")
+
+      (log.l.isInfoEnabled: () => Boolean).expects().returning(false)
+      log.info("baz")
+    }
+    "warn" in {
+      val log = new Slf4jLogger("test") {
+        override val l = mock[slf4j.Logger]
+      }
+
+      val t1 = new Throwable("t1")
+
+      (log.l.isWarnEnabled: () => Boolean).expects().returning(true)
+      (log.l.warn: (String, java.lang.Throwable) => Unit).expects("foo", t1)
+
+      log.warn("foo", t1)
+
+      (log.l.isWarnEnabled: () => Boolean).expects().returning(true)
+      (log.l.warn: (String, java.lang.Throwable) => Unit).expects("bar", null)
+
+      log.warn("bar")
+
+      (log.l.isWarnEnabled: () => Boolean).expects().returning(false)
+      log.warn("baz")
+    }
+    "error" in {
+      val log = new Slf4jLogger("test") {
+        override val l = mock[slf4j.Logger]
+      }
+
+      val t1 = new Throwable("t1")
+
+      (log.l.isErrorEnabled: () => Boolean).expects().returning(true)
+      (log.l.error: (String, java.lang.Throwable) => Unit).expects("foo", t1)
+
+      log.error("foo", t1)
+
+      (log.l.isErrorEnabled: () => Boolean).expects().returning(true)
+      (log.l.error: (String, java.lang.Throwable) => Unit).expects("bar", null)
+
+      log.error("bar")
+
+      (log.l.isErrorEnabled: () => Boolean).expects().returning(false)
+      log.error("baz")
+    }
+  }
+
+  "a Logger" should {
+    "allow messages to be lazy" in {
+      var evaluated: String = ""
+
+      Logger.Silent.trace({evaluated = "trace"; "omg"})
+      Logger.Silent.trace({evaluated = "trace1"; "omg"}, new Throwable)
+      Logger.Silent.trace({evaluated = "trace2"; "omg"}, Some(new Throwable))
+      Logger.Silent.trace({evaluated = "trace3"; "omg"}, None)
+      Logger.Silent.trace({evaluated = "trace4"; this})
+
+      Logger.Silent.debug({evaluated = "debug"; "omg"})
+      Logger.Silent.debug({evaluated = "debug1"; "omg"}, new Throwable)
+      Logger.Silent.debug({evaluated = "debug2"; "omg"}, Some(new Throwable))
+      Logger.Silent.debug({evaluated = "debug3"; "omg"}, None)
+      Logger.Silent.debug({evaluated = "debug4"; this})
+
+      Logger.Silent.info({evaluated = "info"; "omg"})
+      Logger.Silent.info({evaluated = "info1"; "omg"}, new Throwable)
+      Logger.Silent.info({evaluated = "info2"; "omg"}, Some(new Throwable))
+      Logger.Silent.info({evaluated = "info3"; "omg"}, None)
+
+      Logger.Silent.warn({evaluated = "warn"; "omg"})
+      Logger.Silent.warn({evaluated = "warn1"; "omg"}, new Throwable)
+      Logger.Silent.warn({evaluated = "warn2"; "omg"}, Some(new Throwable))
+      Logger.Silent.warn({evaluated = "warn3"; "omg"}, None)
+
+      Logger.Silent.error({evaluated = "error"; "omg"})
+      Logger.Silent.error({evaluated = "error1"; "omg"}, new Throwable)
+      Logger.Silent.error({evaluated = "error2"; "omg"}, Some(new Throwable))
+      Logger.Silent.error({evaluated = "error3"; "omg"}, None)
+
+      evaluated shouldBe ""
+    }
+
+    "log trace" in {
+      // need to mock the function rather than the Logger class because I can't figure out how to handle the
+      // expect/verify of the m: => String arg in the Logger methods via mock[Logger]
+      val mf = mockFunction[String, Option[Throwable], Unit]
+      val l = new Logger {
+        override def trace(m: => String, t: Option[Throwable]): Unit = mf(m, t)
+        override def debug(m: => String, t: Option[Throwable]): Unit = ???
+        override def info(m: => String, t: Option[Throwable]): Unit = ???
+        override def warn(m: => String, t: Option[Throwable]): Unit = ???
+        override def error(m: => String, t: Option[Throwable]): Unit = ???
+      }
+
+      val t = new Throwable
+
+      inSequence {
+        mf.expects("foo", None)
+        mf.expects("bar", Some(t))
+        mf.expects(this.toString(), None)
+        mf.expects("this is the stuff", None)
+      }
+
+      l.trace("foo")
+      l.trace("bar", t)
+      l.trace(this)
+      l.tracing {
+        "this is the stuff"
+      } shouldBe "this is the stuff"
+    }
+
+    "log debug" in {
+      // with a stub (vs mock) you verify after-the-fact rather than expect ahead of time
+      val mf = stubFunction[String, Option[Throwable], Unit]
+      val l = new Logger {
+        override def trace(m: => String, t: Option[Throwable]): Unit = ???
+        override def debug(m: => String, t: Option[Throwable]): Unit = mf(m, t)
+        override def info(m: => String, t: Option[Throwable]): Unit = ???
+        override def warn(m: => String, t: Option[Throwable]): Unit = ???
+        override def error(m: => String, t: Option[Throwable]): Unit = ???
+      }
+
+      val t = new Throwable
+
+      l.debug("foo")
+      l.debug("bar", t)
+      l.debug(this)
+      l.debuging {
+        "this is the stuff"
+      } shouldBe "this is the stuff"
+
+      inSequence {
+        mf.verify("foo", None)
+        mf.verify("bar", Some(t))
+        mf.verify(this.toString(), None)
+        mf.verify("this is the stuff", None)
+      }
+    }
+
+    "log info" in {
+      val mf = stubFunction[String, Option[Throwable], Unit]
+      val l = new Logger {
+        override def trace(m: => String, t: Option[Throwable]): Unit = ???
+        override def debug(m: => String, t: Option[Throwable]): Unit = ???
+        override def info(m: => String, t: Option[Throwable]): Unit = mf(m, t)
+        override def warn(m: => String, t: Option[Throwable]): Unit = ???
+        override def error(m: => String, t: Option[Throwable]): Unit = ???
+      }
+
+      val t = new Throwable
+
+      l.info("foo")
+      l.info("bar", t)
+
+      inSequence {
+        mf.verify("foo", None)
+        mf.verify("bar", Some(t))
+      }
+    }
+
+    "log warn" in {
+      val mf = stubFunction[String, Option[Throwable], Unit]
+      val l = new Logger {
+        override def trace(m: => String, t: Option[Throwable]): Unit = ???
+        override def debug(m: => String, t: Option[Throwable]): Unit = ???
+        override def info(m: => String, t: Option[Throwable]): Unit = ???
+        override def warn(m: => String, t: Option[Throwable]): Unit = mf(m, t)
+        override def error(m: => String, t: Option[Throwable]): Unit = ???
+      }
+
+      val t = new Throwable
+
+      l.warn("foo")
+      l.warn("bar", t)
+
+      inSequence {
+        mf.verify("foo", None)
+        mf.verify("bar", Some(t))
+      }
+    }
+
+    "log error" in {
+      val mf = stubFunction[String, Option[Throwable], Unit]
+      val l = new Logger {
+        override def trace(m: => String, t: Option[Throwable]): Unit = ???
+        override def debug(m: => String, t: Option[Throwable]): Unit = ???
+        override def info(m: => String, t: Option[Throwable]): Unit = ???
+        override def warn(m: => String, t: Option[Throwable]): Unit = ???
+        override def error(m: => String, t: Option[Throwable]): Unit = mf(m, t)
+      }
+
+      val t = new Throwable
+
+      l.error("foo")
+      l.error("bar", t)
+
+      inSequence {
+        mf.verify("foo", None)
+        mf.verify("bar", Some(t))
+      }
     }
   }
 }
