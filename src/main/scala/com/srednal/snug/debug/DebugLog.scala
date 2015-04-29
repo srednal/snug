650a9605473779a578f52df4240e2f70beb625a2
Dave Landers <dave.landers@emc.com>
2015-04-29 10:17:52 -0600

    Add a macro as an experiment - wrap logger.debug with something that logs param name = value

---
+++ src/main/scala/com/srednal/snug/debug/DebugLog.scala
@@ -0,0 +1,49 @@
+package com.srednal.snug.debug
+
+import scala.language.experimental.macros
+import scala.reflect.macros.blackbox
+import com.typesafe.scalalogging.Logger
+
+/** Intended to be mixed-in with, for example, LazyLogging or StrictLogging,
+  * however the only restriction is that logger.debug(String) can be called somehow.
+  */
+trait DebugLog {
+
+  // use a structural type here (rather than typesafe.logging.Logger) because
+  // Logger is final and this makes testing more accessible
+  // scalastyle:ignore structural.type
+  protected def logger: {
+    def debug(message: String): Unit
+    def trace(message: String): Unit
+  }
+
+  // debuglog(foo) => logger.debug(s"foo = $foo")
+  // debuglog(foo, bar) => logger.debug(s"foo = $foo"); logger.debug(s"bar = $bar")
+  // debuglog("foo", bar) => logger.debug("foo"); logger.debug(s"bar = $bar")
+  def debuglog(params: Any*): Unit = macro DebugMacroBundle.debug
+  def tracelog(params: Any*): Unit = macro DebugMacroBundle.trace
+
+}
+
+class DebugMacroBundle(val c: blackbox.Context) {
+
+  // scalastyle:ignore import.grouping
+  import c.universe._
+
+  // call logger.debug once for each param
+  def debug(params: c.Expr[Any]*) =
+    Block(params.toList map paramDebugString map { s => q"${c.prefix}.logger.debug($s)" }, Literal(Constant(())))
+  def trace(params: c.Expr[Any]*) =
+    Block(params.toList map paramDebugString map { s => q"${c.prefix}.logger.trace($s)" }, Literal(Constant(())))
+
+
+  // a constant literal results in simply the value
+  // otherwise fetch the param name and use that in the log as "name = value"
+  def paramDebugString(param: c.Expr[Any]) = param.tree match {
+    case c.universe.Literal(c.universe.Constant(_)) => q"$param.toString"
+    case t =>
+      val name = c.Expr[String](Literal(Constant(show(t))))
+      reify {name.splice + " = " + param.splice}.tree
+  }
+}
+
--- src/test/scala/com/srednal/snug/debug/DebugTest.scala
+++ src/test/scala/com/srednal/snug/debug/DebugTest.scala
@@ -1,28 +1,140 @@
 package com.srednal.snug.debug
 
-import java.util.concurrent.atomic.AtomicReference
-import com.srednal.snug.debug
+import scala.collection.mutable.ListBuffer
+import com.typesafe.scalalogging.{StrictLogging, LazyLogging}
 import org.scalatest._
 
-class DebugTest extends WordSpec with Matchers with debug.Debug {
+// scalastyle:off magic.number multiple.string.literals import.grouping var.field
+object DebugTest {
+  val w = 42
 
   class X {
-    val debugged  = new AtomicReference[String]()  // let someone else be responsible for mutable stuff
-    def debug(s: String) = debugged set s
+    val debugged = ListBuffer[String]()
+    val traced = ListBuffer[String]()
+    def debug(s: String): Unit = debugged += s
+    def trace(s: String): Unit = traced += s
+  }
+
+}
+
+class DebugTest extends WordSpec with Matchers with DebugLog {
+  import DebugTest._
+
+  override val logger = new X
+
+  val x = "the value"
+  def z = 3.14
+  def zz(s: String) = s.reverse
+
+  "the debug macro" should {
+
+    "debug a single constant" in {
+      logger.debugged.clear()
+      debuglog("foo")
+      logger.debugged shouldBe "foo" :: Nil
+    }
+
+    "debug a single non-string constant" in {
+      logger.debugged.clear()
+      debuglog(123)
+      logger.debugged shouldBe "123" :: Nil
+    }
+
+    "debug a local val" in {
+      logger.debugged.clear()
+      val foo = "xyzzy"
+      debuglog(foo)
+      logger.debugged shouldBe "foo = xyzzy" :: Nil
+    }
+    "debug a local var" in {
+      logger.debugged.clear()
+      var foo = "xyzzy"
+      debuglog(foo)
+      logger.debugged shouldBe "foo = xyzzy" :: Nil
+    }
+
+    "debug a field" in {
+      logger.debugged.clear()
+      debuglog(x)
+      logger.debugged shouldBe "DebugTest.this.x = the value" :: Nil
+    }
+
+    "debug a method" in {
+      logger.debugged.clear()
+      debuglog(z)
+      debuglog(zz("foo"))
+      logger.debugged shouldBe "DebugTest.this.z = 3.14" :: """DebugTest.this.zz("foo") = oof""" :: Nil
+    }
+    "debug a function" in {
+      logger.debugged.clear()
+      val f: String => Int = _.toInt
+      debuglog(f)
+      debuglog(f("13"))
+      logger.debugged shouldBe "f = <function1>" :: """f.apply("13") = 13""" :: Nil
+    }
+    "debug a companion object field" in {
+      logger.debugged.clear()
+      debuglog(w)
+      logger.debugged shouldBe "DebugTest.w = 42" :: Nil
+    }
+    "debug several args as multiple logger calls" in {
+      logger.debugged.clear()
+      debuglog(w, x, z)
+      logger.debugged shouldBe "DebugTest.w = 42" :: "DebugTest.this.x = the value" :: "DebugTest.this.z = 3.14" :: Nil
+    }
+  }
+
+  "the trace macro" should {
+
+    "trace a single constant" in {
+      logger.traced.clear()
+      tracelog("foo")
+      logger.traced shouldBe "foo" :: Nil
+    }
+
+    "trace a local val" in {
+      logger.traced.clear()
+      val foo = "xyzzy"
+      tracelog(foo)
+      logger.traced shouldBe "foo = xyzzy" :: Nil
+    }
+    "trace a local var" in {
+      logger.traced.clear()
+      var foo = "xyzzy"
+      tracelog(foo)
+      logger.traced shouldBe "foo = xyzzy" :: Nil
+    }
+
+    "trace a field" in {
+      logger.traced.clear()
+      tracelog(x)
+      logger.traced shouldBe "DebugTest.this.x = the value" :: Nil
+    }
+
+    "trace a method" in {
+      logger.traced.clear()
+      tracelog(z)
+      logger.traced shouldBe "DebugTest.this.z = 3.14" :: Nil
+    }
+    "trace a companion object field" in {
+      logger.traced.clear()
+      tracelog(w)
+      logger.traced shouldBe "DebugTest.w = 42" :: Nil
+    }
+    "trace several args as multiple logger calls" in {
+      logger.traced.clear()
+      tracelog(w, x, z)
+      logger.traced shouldBe "DebugTest.w = 42" :: "DebugTest.this.x = the value" :: "DebugTest.this.z = 3.14" :: Nil
+    }
   }
 
 
-  val x = "the value"
-
-  "the debug macro" should {
-    "debug" in {
-      val logger = new X
-
-      val y = "the other"
-      debug(x, y) // macro expands to: logger.debug("Woot.this.x = the value, Woot.this.y = the other")
-
-      logger.debugged.get shouldBe "DebugTest.this.x = the value, y = the other"
-
+  "Debug" should {
+    "mixin with LazyLogging" in {
+      class Test extends LazyLogging with DebugLog
+    }
+    "mixin with StructLogging" in {
+      class Test extends StrictLogging with DebugLog
     }
   }
 }
