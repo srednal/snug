09b147269feba0a560673c492c48fabc89d58cdf
Dave Landers <dave.landers@emc.com>
2015-07-13 10:35:57 -0600

    ConfigKey should be a case class

--- src/main/scala/com/srednal/snug/config/ConfigKey.scala
+++ src/main/scala/com/srednal/snug/config/ConfigKey.scala
@@ -1,7 +1,3 @@
 package com.srednal.snug.config
 
-class ConfigKey[X: ConfigConversion](val path: String)
-
-object ConfigKey {
-  def apply[X: ConfigConversion](path: String): ConfigKey[X] = new ConfigKey(path)
-}
+case class ConfigKey[X: ConfigConversion](val path: String)
