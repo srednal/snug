21c041bc428881dd9d398746d7faf0b5396fe5fc
Dave Landers <dave@srednal.com>
2015-11-24 11:35:42 -0700

    Make mutableOption traversable so it has map, flatMap, etc.

--- src/main/scala/com/srednal/snug/MutableOption.scala
+++ src/main/scala/com/srednal/snug/MutableOption.scala
@@ -8,7 +8,7 @@ object MutableOption {
   def apply[A](value: A) = new MutableOption[A](value)
 }
 
-final class MutableOption[A] private(initialValue: Option[A]) {
+final class MutableOption[A] private(initialValue: Option[A]) extends Traversable[A] {
   private def this() = this(None)
   private def this(a: A) = this(Option(a))
 
@@ -20,8 +20,7 @@ final class MutableOption[A] private(initialValue: Option[A]) {
 
   def asOption: Option[A] = ref.get
 
-  def isEmpty: Boolean = asOption.isEmpty
-  def nonEmpty: Boolean = !isEmpty
+  override def isEmpty: Boolean = asOption.isEmpty
   def isDefined: Boolean = nonEmpty
 
   def getOrElse[B >: A](default: => B): B = asOption getOrElse default
@@ -30,9 +29,7 @@ final class MutableOption[A] private(initialValue: Option[A]) {
 
   def orNull[A1 >: A](implicit ev: Null <:< A1): A1 = asOption.orNull
 
-  def foreach[U](f: A => U): Unit = asOption foreach f
-
-  def exists(p: A => Boolean): Boolean = asOption exists p
+  override def foreach[U](f: A => U): Unit = asOption foreach f
 
   def getOrElseUpdate(newValue: => A): A = getOrElse {
     val v = newValue
